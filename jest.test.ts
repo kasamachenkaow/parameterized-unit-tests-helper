561072e5d3d53177bc0bb6532cdee82af9d16191
kasamachenkaow <kasama.chenkaow@gmail.com>
2020-09-20 01:05:34 +0200

    Add jest support

---
+++ jest.test.ts
@@ -0,0 +1,56 @@
+import "reflect-metadata";
+import { expect } from "@jest/globals";
+
+import {
+  testsuite,
+  testname,
+  testcase,
+  before,
+  after,
+  beforeEach,
+  afterEach,
+} from "./jest";
+
+@testsuite("Example test suite #1")
+@before(() => console.log("Before #1"))
+@after(() => console.log("After #1"))
+export class DumpCalculatorTest1 {
+  @testcase(1, 2, 3)
+  @testcase(2, 2, 4)
+  superDumpPlusTest(a: number, b: number, expected: number): void {
+    const r = a + b;
+    expect(r).toEqual(expected);
+    console.log(r);
+  }
+}
+
+@testsuite("Example test suite #2")
+@before(() => console.log("Before #2"))
+@after(() => console.log("After #2"))
+@beforeEach(() => console.log("Before each #2"))
+@afterEach(() => console.log("After each #2"))
+export class DumpCalculatorTest2 {
+  @testname("Example test case #2")
+  @testcase(1, 2, 3)
+  @testcase(2, 2, 4)
+  superDumpPlusTest(a: number, b: number, expected: number): void {
+    const r = a + b;
+    expect(r).toEqual(expected);
+    console.log(r);
+  }
+}
+
+@testsuite("Example test suite #3")
+@before(() => console.log("Before #3"))
+@after(() => console.log("After #3"))
+@beforeEach(() => console.log("Before each #3"))
+@afterEach(() => console.log("After each #3"))
+export class DumpCalculatorTest3 {
+  @testname("Example test case #3")
+  @testcase(1, 2, 3)
+  @testcase(2, 2, 4)
+  superDumpPlusTest(a: number, b: number, expected: number): void {
+    const r = a + b;
+    expect(r).toEqual(expected);
+  }
+}
--- jest.ts
+++ jest.ts
@@ -1,13 +1,36 @@
-// import { describe, test } from '@jest/globals'
+import Jest from "@jest/globals";
 
-// import {
-//   testnameBuilder,
-//   testcaseBuilder,
-//   testsuiteBuilder,
-// } from './src/decorators'
+import {
+  testnameBuilder,
+  testcaseBuilder,
+  testsuiteBuilder,
+  beforeBuilder,
+  afterBuilder,
+  beforeEachBuilder,
+  afterEachBuilder,
+} from "./src/decorators";
 
-// export default {
-//   testname: testnameBuilder(),
-//   testcase: testcaseBuilder(),
-//   testsuite: testsuiteBuilder(describe, test),
-// }
+export const testname = testnameBuilder();
+export const testcase = testcaseBuilder();
+export const before = beforeBuilder();
+export const after = afterBuilder();
+export const beforeEach = beforeEachBuilder();
+export const afterEach = afterEachBuilder();
+export const testsuite = testsuiteBuilder(
+  Jest.describe,
+  Jest.test,
+  Jest.beforeAll,
+  Jest.afterAll,
+  Jest.beforeEach,
+  Jest.afterEach,
+);
+
+export default {
+  testname,
+  testcase,
+  beforeEach,
+  afterEach,
+  before,
+  after,
+  testsuite,
+};
